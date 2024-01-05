9be6cb58531e9e961ad554a0054841d219282762
LiduinBos <155640655+LiduinBos@users.noreply.github.com>
2024-01-05 11:49:50 +0000

    add start and end date menu to app in radiation checker

--- pages/1_radiation_data_checker.py
+++ pages/1_radiation_data_checker.py
@@ -13,7 +13,6 @@ https://docs.streamlit.io/streamlit-community-cloud/get-started/quickstart
 ## import needed packages
 import streamlit as st
 
-# import plotly as pl
 import plotly.io as pio
 import plotly.express as px
 
@@ -21,10 +20,24 @@ import requests
 import pandas as pd
 import io
 
+import datetime
+
+# set start and end date
+# make it variables that can be chosen in the app as a given range
+date = "20231201"
+today = datetime.date.today()
+history = today - datetime.timedelta(days=3)
+end_date = st.date_input('Start date', today)
+start_date = st.date_input('End date', history)
+if start_date < end_date:
+    st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
+else:
+    st.error('Error: End date must fall after start date.')
+
 # Download data files
 # by making use of the requests python package
+# and for the selected date range
 pars = "cnr4"
-date = "20231201"
 url = "http://liduin:WunDer@2024;@majisysdemo.itc.utwente.nl/wunder/logger_files/Wenumseveld_EC/CR3000_Wenumseveld_"+pars+"_ts"+date+".dat"
 username = 'liduin'
 password = 'WunDer@2024;'
@@ -44,9 +57,6 @@ pio.renderers.default='browser'
 pd.options.plotting.backend = "plotly"
 # pio.templates.default = "plotly"
 fig = df2.plot(x='TIMESTAMP',y=['Rs_in','Rs_out','Rl_in','Rl_out'])
-# fig = px.line(df2,x='TIMESTAMP',y=['Rs_in','Rs_out','Rl_in','Rl_out'])
-
-# fig.show()
 
 ## create simple dashboard
 st.plotly_chart(fig)
