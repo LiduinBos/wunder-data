4361353a82f059ef36bfc9c6bff1cf6768fc0530
LiduinBos <155640655+LiduinBos@users.noreply.github.com>
2024-01-08 08:09:00 +0000

    update x axis range

--- pages/1_radiation_data_checker.py
+++ pages/1_radiation_data_checker.py
@@ -71,6 +71,7 @@ pd.options.plotting.backend = "plotly"
 # pio.templates.default = "plotly"
 fig = df_all.plot(x='TIMESTAMP',y=['Rs_in','Rs_out','Rl_in','Rl_out'])
 fig.update_layout(hovermode="x unified")
+fig.update_xaxes(range = [start_date,end_date])
 
 ## create simple dashboard
 st.plotly_chart(fig)
