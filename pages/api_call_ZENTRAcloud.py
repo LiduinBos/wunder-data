f69b0ee38fbfff50b6defc71984625051b6b806a
LiduinBos <155640655+LiduinBos@users.noreply.github.com>
2024-10-31 14:36:44 +0100

    Update api_call_ZENTRAcloud.py

--- pages/api_call_ZENTRAcloud.py
+++ pages/api_call_ZENTRAcloud.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import pandas
 
 # Streamlit app title
 st.title('ZENTRA Cloud API Caller')
@@ -50,8 +51,18 @@ server="https://zentracloud.com"
 
 if st.button('Make API Call'):
     try:
-        df = get_readings_dataframe(sn, start_date, end_date, token=tok, server=server)
-        print(df)
+       # data = eval(data_input) 
+       df = get_readings_dataframe(sn, start_date, end_date, token=tok, server=server)
+        # print(df)
+
+
+        # Check the response
+        if response.status_code == 200:
+            st.success('API Call Successful!')
+            st.json(response.json())
+        else:
+            st.error(f'Error: {response.status_code} - {response.text}')
+
     except Exception as e:
         st.error(f'An error occurred: {e}')
 
@@ -76,49 +87,3 @@ if st.button('Make API Call'):
 
 #     except Exception as e:
 #         st.error(f'An error occurred: {e}')
-
-
-#import requests # pip install requests
-#import pandas # pip install pandas
-
-#def get_with_credentials(tok, uri, **kwargs):
-#    # just in case we forget to put "Token MyToken"
-#    token = tok if tok.lower().startswith("token") else f"Token {tok}"
-#    token = 'f9e6d698624c76340adde78b22a9c6ff514c6e42'
-#    headers = {"Authorization": token}
-#    return requests.get(uri, headers=headers, **kwargs)
-
-
-#def get_readings_response(sn, start_date, end_date, **extra_kwargs_for_endpoint):
-#     server = extra_kwargs_for_endpoint.get("server", "https://zentracloud.com")
-#     url = f"{server}/api/v4/get_readings/"
-
-#     default_args = {
-#         'output_format': "df",
-#         'per_page': 1,
-#         'page_num': 1,
-#         'sort_by': 'desc',
-#         'start_date': start_date,
-#         'end_date': end_date
-#     }
-#     data = {**default_args, **extra_kwargs_for_endpoint, "device_sn": sn}
-#     tok = extra_kwargs_for_endpoint.pop("token", "")
-#     return get_with_credentials(tok, url, params=data)
-
-# def get_readings_dataframe(sn,start_date,end_date,**extra_kwargs_for_endpoint):
-#     res = get_readings_response(sn,start_date,end_date,**extra_kwargs_for_endpoint)
-#     if(res.ok):
-#         data = res.json()
-#         return pandas.DataFrame(**json.loads(data["data"]))
-#     return res
-
-# #fill in your token, device serial number and start and end dates here.
-# tok = "f9e6d698624c76340adde78b22a9c6ff514c6e42"
-# sn="z6-"+stnr
-# start_date="2024-04-01 00:00:00"
-# end_date="2024-04-02 00:00:00"
-
-# #specify the server here.
-# server="https://zentracloud.com"
-# df = get_readings_dataframe(sn, start_date, end_date, token=tok, server=server)
-# print(df)
