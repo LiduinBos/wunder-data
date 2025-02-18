2e5de14247c318805e5d7d336ad5a2f1e468e971
LiduinBos <liduin.bos-burgering@deltares.nl>
2025-02-18 11:30:09 +0100

    Update 1_Radiation.py
    adjusting labels

--- pages/1_Radiation.py
+++ pages/1_Radiation.py
@@ -76,11 +76,15 @@ df_all['LWTop_cor'] = pd.to_numeric(df_all['LWTopC_Avg'])
 df_all['LWBottom_cor'] = pd.to_numeric(df_all['LWBottomC_Avg'])
 df_all['Rn'] =  (df_all['SWTop']-df_all['SWBottom'])+(df_all['LWTop_cor']-df_all['LWBottom_cor'])
 
+# rename columns
+# df_all.rename(columns={"SWTop":"Incoming short wave radiation","SWBottom":"Outcoming short wave radiation","LWTop_cor":"Corrected incoming long wave radiation","LWBottom_cor":"Corrected outcoming long wave radiation"})
+
 ## plot with plotly
 pio.renderers.default='browser'
 pd.options.plotting.backend = "plotly"
 # pio.templates.default = "plotly"
-fig = df_all.plot(x='TIMESTAMP',y=['SWTop','SWBottom','LWTop_cor','LWBottom_cor']) #,'Rn'])
+fig = df_all.plot(x='TIMESTAMP',y=['SWTop','SWBottom','LWTop_cor','LWBottom_cor'],
+                  labels={"SWTop":"Incoming short wave radiation","SWBottom":"Outcoming short wave radiation","LWTop_cor":"Corrected incoming long wave radiation","LWBottom_cor":"Corrected outcoming long wave radiation"}) #,'Rn'])
 fig.update_layout(hovermode="x unified",xaxis_title=None,yaxis_title='Radiation [W/m2]')
 ## set date range maximum on end_date + 1
 if end_date==today:
